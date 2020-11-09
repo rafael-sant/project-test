fdc0abd5a31c160d1e28d7255d338bf63b8cf1a0
Your Name <you@example.com>
2020-11-09 01:20:05 -0300

    Styling elements and adding new features about the interation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,31 @@
+function addValueToInput() {
+    $(".number ul li").not("#featured, #historic, #back").click(function(){
+        
+        var value = $("#visor").val();
+        var newValue = value + $(this).text(); 
+        $("#visor").val(newValue);
+    })
+}
+
+function deleteNumber() {
+    $(".number ul li#back").click(function(){
+        var value = $("#visor").val();
+        $("#visor").val(value.substring(0, value.length - 1))
+    });
+}
+
+function showHistoric() {
+    $("#historic").click(function(){
+        $(".historic").addClass("active")
+    })
+    
+ 
+}
+
+
 $(document).ready(function(){
+    addValueToInput();
+    deleteNumber();
+    showHistoric();
+
 })
